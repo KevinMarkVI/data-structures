657be2598de992fa4fe845ef37419a162d5547a1
MKS-Student <mks.student@hackreactor.com>
2015-03-05 10:16:00 -0800

    completed through functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,38 @@
 var Queue = function(){
+	var newQueue = {}; 
+	newQueue.storage = {};
+
+	newQueue.upperIndex = 0;
+	newQueue.lowerIndex = 0;
+	extend(newQueue, queueMethods);
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return newQueue;
+};
+
+var extend = function (to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+	this.storage[this.upperIndex] = value;
+	this.upperIndex++;
+	return this.storage;
+};
 
+queueMethods.dequeue = function(){
+	if (this.lowerIndex < this.upperIndex) {
+		var popped = this.storage[this.lowerIndex];
+		delete this.storage[this.lowerIndex];
+		this.lowerIndex++;
+		return popped;
+	}
+};	
 
+queueMethods.size = function(){
+	return (this.upperIndex - this.lowerIndex);
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,23 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var newStack = {};
+	newStack.storage = {};
+	newStack.idx = 0;
+	extend(newStack, stackMethods);
+
+	return newStack;
 };
 
-var stackMethods = {
-
-	var storage = {};
-	var index = 0;
-	
-	var push = function (){
-		
-	};
-
-	var pop = function () {};
-
-	var size = function () {};
-
-
-
+var extend = function (to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
 };
 
+var stackMethods = {};
+ 
+stackMethods.push = function (value){
+	this.storage[this.idx] = value;
+	this.idx++;
+	return this.storage;
+};
+
+stackMethods.pop = function () {
+	if (this.idx > 0) {
+		this.idx--;
+		var popped = this.storage[this.idx];
+		delete this.storage[this.idx];
+		return popped;
+	}			
+};
+
+stackMethods.size = function () {
+	return this.idx;
+};
 
